2fa45c7a71bd1920a9ae058552ab8db3cac8d069
sharisseji <sharisse.wang.ji@gmail.com>
2025-03-17 18:59:46 -0400

    first commit

---
+++ Telemetry_client.py
@@ -0,0 +1,26 @@
+# TEST.PY 
+import grpc
+import host_pb2
+import host_pb2_grpc
+import threading
+
+def send_telemetry(stub):
+    """Continuously sends telemetry data."""
+    def generator():
+        while True:
+            status = input("[Telemetry] Enter status update: ")
+            yield host_pb2.HostMessage(sender="Telemetry", recipient="Dashboard", command=status)
+
+    for _ in stub.TelemetryStream(generator()):
+        pass
+
+def telemetry_client():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = host_pb2_grpc.HostControlStub(channel)
+
+        telemetry_thread = threading.Thread(target=send_telemetry, args=(stub,))
+        telemetry_thread.start()
+        telemetry_thread.join()
+
+if __name__ == '__main__':
+    telemetry_client()
